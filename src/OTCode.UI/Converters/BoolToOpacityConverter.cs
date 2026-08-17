4edd4875bcee37fdf1da0698437758100e13760e
Jing Ming Tan <jingming.tan12@gmail.com>
2026-08-18 00:40:13 +0800

    commit

--- src/OTCode.UI/Converters/BoolToOpacityConverter.cs
+++ src/OTCode.UI/Converters/BoolToOpacityConverter.cs
@@ -1,5 +1,9 @@
 // Copyright (c) Tan Jing Ming. Use of this software is governed by LICENSE.md.
 
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
 namespace OTCode.UI.Converters;
 
 public sealed class BoolToOpacityConverter : IValueConverter
@@ -9,8 +13,8 @@ public sealed class BoolToOpacityConverter : IValueConverter
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         => value is bool b
-            ? (b ? 0.4 : 1.0)
-            : AvaloniaProperty.UnsetValue;
+            ? (b ? 0.4d : 1.0d)
+            : DependencyProperty.UnsetValue;
     
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotSupportedException();
