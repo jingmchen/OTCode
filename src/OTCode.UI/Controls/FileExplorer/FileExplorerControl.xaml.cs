d3fa92120e9502643f03b83df45a7ec1d19ac999
Jing Ming Tan <jingming.tan12@gmail.com>
2026-08-23 00:57:16 +0800

    commit

---
+++ src/OTCode.UI/Controls/FileExplorer/FileExplorerControl.xaml.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Tan Jing Ming. Use of this software is governed by LICENSE.md.
+
+using System.Windows.Controls;
+
+namespace OTCode.UI.Controls.FileExplorer;
+
+public sealed partial class FileExplorerControl : UserControl
+{
+}
